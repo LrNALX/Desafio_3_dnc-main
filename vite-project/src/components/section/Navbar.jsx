0b43c33e213469f9d11f8b2a48aca2a874a72a63
LrNALX <lorenzo.aleixo@hotmail.com>
2024-05-15 14:22:20 -0300

    atualizações

--- vite-project/src/components/section/Navbar.jsx
+++ vite-project/src/components/section/Navbar.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import Tech from '../elements/Tech'
 import { FaGithub } from 'react-icons/fa6';
 import { FaLinkedin } from 'react-icons/fa6';
+import Nav from 'react-bootstrap/Nav'
 
 
 
@@ -18,21 +19,25 @@ function Navbar() {
         <><nav className={styles.navbar}>
             <div className={styles.links}>
                 <ul>
-                    <li>Projeto</li>
-                    <li>Sobre mim</li>
-                    <li>Tecnologias</li>
+                <li><Nav.Link href='#projects'>Projetos</Nav.Link></li>
+                    <li><Nav.Link onClick={handleToggleTechnologies}>Tecnologias</Nav.Link></li>
+                    <li><Nav.Link href='#timeline'>Sobre mim</Nav.Link></li>
+                    
                 </ul>
             </div>
             <div className={styles.icons}>
                 <ul>
                     <li><a target="_blank" href="https://github.com/LrNALX"><FaGithub className={styles.iconSize} /></a></li>
                     <li><a target="_blank" href="https://www.linkedin.com/in/lorenzo-aleixo-192338264/"><FaLinkedin className={styles.iconSize} /></a></li>
-                </ul>
+                    </ul>
             </div>
 
         </nav>
-
-        
+            {showTechnologies && (
+                <div>
+                    <Tech />
+                </div>
+            )}
         </>
     );
 }
